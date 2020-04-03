cd84ff80392cac2285d4af237b96a25222628bc4
EduardoPapandrea <eduardops5@al.insper.edu.br>
2020-04-02 23:24:05 -0300

    começo do while

--- craps_ep1.py
+++ craps_ep1.py
@@ -11,3 +11,15 @@ print('Bem vindo(a) ao CRAPS INSPER!')
 
 Point = False
 fase = 'Comeout' 
+
+#Função While que roda o jogo enquanto o jogador possui dinheiro para apostar
+while player_money > 0:
+    if fase == 'Comeout':
+        game = input('\nIniciar o jogo (sim/não) ')
+    if game == 'sim':
+        #Mostra ao jogador a quantia inicial de dinheiro
+        print('Seu dinheiro total é: {}'.format(player_money))
+        if fase == 'Comeout':
+            #Avisa ao jogador que ele está na fase Come out
+            print('\nVocê está na fase COME OUT ')
+            #Pergunta ao jogador qual tipo de aposta ele deseja realizar
