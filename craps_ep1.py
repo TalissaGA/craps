9ffb297b99c52d694caace840a1ec0da3d8b4bde
Talissa Gonçalves Albertini <talissa.albertini@hotmail.com>
2020-04-02 23:28:40 -0300

    Primeira parte Pass line bet - ComeOut

--- craps_ep1.py
+++ craps_ep1.py
@@ -23,3 +23,41 @@ while player_money > 0:
             #Avisa ao jogador que ele está na fase Come out
             print('\nVocê está na fase COME OUT ')
             #Pergunta ao jogador qual tipo de aposta ele deseja realizar
+            if fase == 'Comeout':
+            #Avisa ao jogador que ele está na fase Come out
+            print('\nVocê está na fase COME OUT ')
+            #Pergunta ao jogador qual tipo de aposta ele deseja realizar
+            tipo = str(input('Qual tipo de aposta deseja realizar? (Pass Line Bet, Field, Any Craps, Twelve) '))
+            #Tipo Pass Line Bet na fase Come out
+            if tipo == 'Pass' or tipo=='pass' or tipo=='pass line bet':
+                #Mostra ao jogador a quantia atual de dinheiro
+                print('Seu dinheiro atual é: {}'.format(player_money))
+                #Pergunta ao jogador quanto deseja apostar
+                bet = int(input('Quanto deseja apostar? '))
+                #Sorteio dos dados 
+                d1 = random.randint(1,6)
+                d2 = random.randint(1,6)
+                soma = d1+d2
+                #Mostra a soma dos dados
+                print('A soma deu {}'.format(soma))
+                #Relaciona a soma dos dados com as regras do tipo Pass Line Bet
+                if soma == 7 or soma == 11:
+                    print('Parabéns! Você Ganhou!\n')
+                    #Caso ele ganhe, adiciona-se o valor da aposta à sua quantia atual de dinheiro
+                    player_money += bet
+                    #Retorna à fase Come Out, para outra rodada
+                    fase = 'Comeout'
+                    print('Seu dinheiro atual é: {}'.format(player_money))
+                if soma == 2 or soma == 3 or soma == 12:
+                    print('CRAPS! Você perdeu!')
+                    #Caso ele Perca, subtrai-se o valor da aposta à sua quantia atual de dinheiro
+                    player_money -= bet
+                    #Retorna à fase Come Out, para outra rodada
+                    fase = 'Comeout'
+                if soma==4 or soma==5 or soma==6 or soma==8 or soma==9 or soma==10:
+                    #Ativa o While do Point
+                    Point = True
+                    #O valor do point é o valor da soma sorteada no Come out
+                    point = soma
+                    #Avisa ao jogador que está indo para a fase Point
+                    print('Próxima fase : POINT!') 
