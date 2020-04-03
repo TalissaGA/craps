204d6c7b3d0ee29a6a6f1a3020370239bdf00161
Talissa Gonçalves Albertini <talissa.albertini@hotmail.com>
2020-04-02 22:55:25 -0300

    Introdução ao jogo

---
+++ craps_ep1.py
@@ -0,0 +1,13 @@
+##Jogo Craps 
+
+#Importar Biblioteca
+import random 
+
+#Dinheiro Inicial do jogador
+player_money = 100
+
+#Introdução ao jogo
+print('Bem vindo(a) ao CRAPS INSPER!')
+
+Point = False
+fase = 'Comeout' 
